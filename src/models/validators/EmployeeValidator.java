7f95f2cd0b194b02472be7b64188dbecf71f2f23
kaneishi <sopoyabu@mbox.re>
2020-05-31 19:46:48 +0900

    Add Employee create

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -40,7 +40,7 @@ public class EmployeeValidator {
         //既に登録されている社員番号との重複チェック
         if(code_dupulicate_check_flag) {
             EntityManager em =DBUtil.createEntityManager();
-            long employees_count = (long)em.createNamedQuery("checkRegisteredCode", long.class)
+            long employees_count = (long)em.createNamedQuery("checkRegisteredCode", Long.class)
                                           .setParameter("code", code)
                                             .getSingleResult();
             em.close();
